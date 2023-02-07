8d4c32025ae684b63348233dc156a9fa903dd7a2
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-02-07 23:30:13 +0100

    more or less detection

--- imgrecog.py
+++ imgrecog.py
@@ -12,10 +12,17 @@ def lin_equ(l1, l2):
     c = (l2[1] - (m * l2[0]))
     return m, c
 
-def image_remove_shadow(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
-    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+def shadow_remove(img):
+    rgb_planes = cv2.split(img)
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_norm_planes.append(norm_img)
+    shadowremov = cv2.merge(result_norm_planes)
+    return shadowremov
 
 def image_addaptive_guassian_threshold(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -36,29 +43,75 @@ def gray_to_bgr(img):
     return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
 if __name__ == '__main__':
-    camera = FakeCamera("test2.jpeg", width=400, height=400)
+    camera = FakeCamera("test.jpg", width=400, height=400)
+
+    vals_type = ["int", "int", "float", "float", "float", "float", "float", "float"]
+    vals = [2, 3, 0.04, 0.01, 0, 0, 0, 0]
 
     while True:
-        ret, frame = camera.get_frame()
-        empty = frame
+        imgs = []
+        camera = FakeCamera("test.jpg", width=400, height=400)
+        camera2 = FakeCamera("test2.jpeg", width=400, height=400)
+        for cam in [camera, camera2]:
+            ret, frame = cam.get_frame()
+            img = frame.copy()
+            empty = frame
 
-        otsu = gray_to_bgr(image_otsu_threshold(frame))
-
-        for img in [frame, otsu]:
             # find Harris corners
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (5, 5), 0)
-            dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+            dst = cv2.cornerHarris(gray, 2, 3, 0.02)
             #result is dilated for marking the corners, not important
             dst = cv2.dilate(dst, None)
-            # Threshold for an optimal value, it may vary depending on the image.
-            img[dst > 0.01 * dst.max()] = [0, 0, 255]
 
+            # find centroids
+            ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+            dst = np.uint8(dst)
+            ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+            # draw centroids
+            #for i in range(1, len(centroids)):
+            #    cv2.circle(img, (int(centroids[i][0]), int(centroids[i][1])), 1, (0, 0, 255), -1)
+
+            # define the criteria to stop and refine the corners
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+            corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+
+            for i in range(1, len(corners)):
+                cv2.circle(img, (int(corners[i][0]), int(corners[i][1])), 1, (0, 0, 255), -1)
+            
+
+            # show the images 2 by 2 in same window
+            imgs.append(np.hstack((frame, img)))
 
         # show the images 2 by 2 in same window
-        cv2.imshow('frame', np.hstack((frame, otsu)))
+        cv2.imshow("img", np.vstack(imgs))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        up = ["w", "e", "r", "t", "y", "u", "i", "o"]
+        down = ["s", "d", "f", "g", "h", "j", "k", "l"]
+
+        key = cv2.waitKey(1)
+
+        if key in [ord(x) for x in up]:
+            index = up.index(chr(key))
+            if index == 1:
+                vals[index] += 2
+                vals[index] = min(31, vals[index])
+            else:
+                vals[index] += 1 if vals_type[index] == "int" else 0.01
+
+        if key in [ord(x) for x in down]:
+            index = down.index(chr(key))
+            if index == 1:
+                vals[index] -= 2
+                vals[index] = max(1, vals[index])
+            else:
+                vals[index] -= 1 if vals_type[index] == "int" else 0.01
+                vals[index] = max(0, vals[index])
+
+        print(vals)
+
+        if key & 0xFF == ord('q'):
             break
 
 
