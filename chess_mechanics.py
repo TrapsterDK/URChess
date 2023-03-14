2216f61530de2014cb20e347bbcf798dad5e0889
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2023-03-14 20:00:12 +0100

    lol

--- chess_mechanics.py
+++ chess_mechanics.py
@@ -71,74 +71,74 @@ class Chess:
         return self.engine.get_best_move_depth(self.depth)
     
         
-    def letter_to_number(self, letter):
-        if letter == "a":
-            return 0
-        elif letter == "b":
-            return 1
-        elif letter == "c":
-            return 2
-        elif letter == "d":
-            return 3
-        elif letter == "e":
-            return 4
-        elif letter == "f":
-            return 5
-        elif letter == "g":
-            return 6
-        elif letter == "h":
-            return 7
-    def number_to_letter(self, number):
-        if number == 0:
-            return "a"
-        elif number == 1:
-            return "b"
-        elif number == 2:
-            return "c"
-        elif number == 3:
-            return "d"
-        elif number == 4:
-            return "e"
-        elif number == 5:
-            return "f"
-        elif number == 6:
-            return "g"
-        elif number == 7:
-            return "h"
-    def start_pos(self):
-        #self.engine.set_fen("rnbqkbnr1pppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-        self.piece_positions[0][0] = 1
-        self.piece_positions[0][1] = 1
-        self.piece_positions[0][2] = 1
-        self.piece_positions[0][3] = 1
-        self.piece_positions[0][4] = 1
-        self.piece_positions[0][5] = 1
-        self.piece_positions[0][6] = 1
-        self.piece_positions[0][7] = 1
-        self.piece_positions[1][0] = 1
-        self.piece_positions[1][1] = 1
-        self.piece_positions[1][2] = 1
-        self.piece_positions[1][3] = 1
-        self.piece_positions[1][4] = 1
-        self.piece_positions[1][5] = 1
-        self.piece_positions[1][6] = 1
-        self.piece_positions[1][7] = 1
-        self.piece_positions[6][0] = 1
-        self.piece_positions[6][1] = 1
-        self.piece_positions[6][2] = 1
-        self.piece_positions[6][3] = 1
-        self.piece_positions[6][4] = 1
-        self.piece_positions[6][5] = 1
-        self.piece_positions[6][6] = 1
-        self.piece_positions[6][7] = 1
-        self.piece_positions[7][0] = 1
-        self.piece_positions[7][1] = 1
-        self.piece_positions[7][2] = 1
-        self.piece_positions[7][3] = 1
-        self.piece_positions[7][4] = 1
-        self.piece_positions[7][5] = 1
-        self.piece_positions[7][6] = 1
-        self.piece_positions[7][7] = 1
+def letter_to_number(self, letter):
+    if letter == "a":
+        return 0
+    elif letter == "b":
+        return 1
+    elif letter == "c":
+        return 2
+    elif letter == "d":
+        return 3
+    elif letter == "e": 
+        return 4
+    elif letter == "f":
+        return 5
+    elif letter == "g":
+        return 6
+    elif letter == "h":
+        return 7
+def number_to_letter(self, number):
+    if number == 0:
+        return "a"
+    elif number == 1:
+        return "b"
+    elif number == 2:
+        return "c"
+    elif number == 3:
+        return "d"
+    elif number == 4:
+        return "e"
+    elif number == 5:
+        return "f"
+    elif number == 6:
+        return "g"
+    elif number == 7:
+        return "h"
+def start_pos(self):
+    #self.engine.set_fen("rnbqkbnr1pppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    self.piece_positions[0][0] = 1
+    self.piece_positions[0][1] = 1
+    self.piece_positions[0][2] = 1
+    self.piece_positions[0][3] = 1
+    self.piece_positions[0][4] = 1
+    self.piece_positions[0][5] = 1
+    self.piece_positions[0][6] = 1
+    self.piece_positions[0][7] = 1
+    self.piece_positions[1][0] = 1
+    self.piece_positions[1][1] = 1
+    self.piece_positions[1][2] = 1
+    self.piece_positions[1][3] = 1
+    self.piece_positions[1][4] = 1
+    self.piece_positions[1][5] = 1
+    self.piece_positions[1][6] = 1
+    self.piece_positions[1][7] = 1
+    self.piece_positions[6][0] = 1
+    self.piece_positions[6][1] = 1
+    self.piece_positions[6][2] = 1
+    self.piece_positions[6][3] = 1
+    self.piece_positions[6][4] = 1
+    self.piece_positions[6][5] = 1
+    self.piece_positions[6][6] = 1
+    self.piece_positions[6][7] = 1
+    self.piece_positions[7][0] = 1
+    self.piece_positions[7][1] = 1
+    self.piece_positions[7][2] = 1
+    self.piece_positions[7][3] = 1
+    self.piece_positions[7][4] = 1
+    self.piece_positions[7][5] = 1
+    self.piece_positions[7][6] = 1
+    self.piece_positions[7][7] = 1
     def get_coord(self, move):
         return str(self.letter_to_number(move[0])) + str(int(move[1])-1) + str(self.letter_to_number(move[2])) + str(int(move[3])-1)
     def undo(self):
